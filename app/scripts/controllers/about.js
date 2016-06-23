2c3a93815f3e3b178a0178ec4c58392297deac76
okita <okita@macbook.local>
2016-06-23 16:57:25 +0900

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yotestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yotestApp
+ */
+angular.module('yotestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
